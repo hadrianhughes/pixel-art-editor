a06fde8b48b77ee1f8a0f26830ada7dbce4cb741
Hadrian Hughes <h.hughes@amaze.com>
2018-10-21 11:25:38 +0100

    Basic Hello World rendering

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,25 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   devtool: 'sourcemap',
   entry: './src/index.js',
   module: {
     rules: [
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
       },
     ],
   },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'static' },
+    ]),
+  ],
   output: {
     path: path.join(__dirname, '/build'),
     filename: 'bundle.js',
